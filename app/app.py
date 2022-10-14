5740dee0348fca93e7a4168b3cc1f9fd1502b5d0
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:01:26 +0200

    s

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import sys
 args = str(sys.argv)
+args.pop(0)
 print(args)
 if (args == []):
     print("\nWelcome to dts man\nTo start, the arguments for this program are\n1. dts path file to create\n2.http server link to host at\nexample: dtsman C:\\myrobloxproject\\objects.d.ts localhost://8080")
