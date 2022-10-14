057784b2e022731d5280e52731a7788aded92e7b
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:33:21 +0200

    ok

--- plugin/components/dtsgui.lua
+++ plugin/components/dtsgui.lua
@@ -1,2 +1,9 @@
 local packages = script.Parent.Parent:WaitForChild("packages")
 local roact = require(packages:WaitForChild("roact"))
+local component = roact.Component:extend("dtsgui")
+function component:render()
+    return roact.createElement("Frame", {
+        Size = UDim2.fromScale(1, 1),
+        
+    })
+end
