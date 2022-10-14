cd3979fd20d4fa28010dc9d3f702c218c3615bb8
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:34:08 +0200

    ok

--- plugin/components/dtsgui.lua
+++ plugin/components/dtsgui.lua
@@ -4,6 +4,6 @@ local component = roact.Component:extend("dtsgui")
 function component:render()
     return roact.createElement("Frame", {
         Size = UDim2.fromScale(1, 1),
-        
+        BackgroundColor3 = Color3.fromRGB(4, 21, 144)
     })
 end
