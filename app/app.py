d4ce1fc49b2d992743846d57f731f09e1339a4df
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:04:56 +0200

    commit

--- app/app.py
+++ app/app.py
@@ -3,3 +3,7 @@ args = sys.argv
 args.pop(0)
 if (args == []):
     print("\nWelcome to dts man\nTo start, the arguments for this program are\n1. dts path file to create\n2.http server link to host at\nexample: dtsman C:\\myrobloxproject\\objects.d.ts localhost://8080")
+else:
+    path = args[0]
+    httpServer = args[1]
+    print("Hosting at {httpServer} and waiting for response to write {path}\nUse the roblox plugin and type in the http link and select the object you want to convert into .d.ts and tada\npress ctrl + C to stop\n")
