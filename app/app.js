a9a6c8b667bac29dd9ec4f40d52c3366fb20ee8b
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:16:50 +0200

    writing app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,10 @@
 const args = process.argv.slice(2)
 if (args == []){
     console.log("Welcome to dtsman\nStart automatically creating your .d.ts file by doing\n node dtsman C:\\exampleProject\\objects.d.ts 8080\nthe first argument: path of .d.ts file to write, second is simply the localhost port you want.")
+} else {
+    const path = args[0]
+    const httpPort = args[1]
+    const express = require("express")
+    const app = express()
+    
 }
