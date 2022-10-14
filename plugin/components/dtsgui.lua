b7cb6ae84a1a54c942a5963b5c651462a8b8707c
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:34:53 +0200

    s

--- plugin/components/dtsgui.lua
+++ plugin/components/dtsgui.lua
@@ -5,5 +5,7 @@ function component:render()
     return roact.createElement("Frame", {
         Size = UDim2.fromScale(1, 1),
         BackgroundColor3 = Color3.fromRGB(4, 21, 144)
+    }, {
+        
     })
 end
