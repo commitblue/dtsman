7e459169cc36ef284f2349db70326d7118241797
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-14 20:25:35 +0200

    s

--- plugin/main.server.lua
+++ plugin/main.server.lua
@@ -1,1 +1,5 @@
 local httpService = game:GetService("HttpService")
+local packages = script.Parent:WaitForChild("packages")
+local roact = require(packages:WaitForChild("roact"))
+local roactrodux = require(packages:WaitForChild("roact-rodux"))
+local rodux = require(packages:WaitForChild("rodux"))
