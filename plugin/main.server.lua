293f9cdf06cefca487f24b923014933880d8328b
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-16 19:37:42 +0200

    ok

--- plugin/main.server.lua
+++ plugin/main.server.lua
@@ -101,12 +101,15 @@ function getGlobalFromString(fullName)
 
 	return current
 end
+local function escape(str)
+    return str:gsub("\"", "\\\"")
+end
 local function generateDtsCode(obj : Instance)
     local generated = string.format("type %s = %s & {\n   ", obj.Name, obj.ClassName)
     local function subFunction_dump(obj)
         local children = obj:GetChildren()
         for _, v in children do
-            generated = generated.."   [\""..v.Name.."\"] : "..v.ClassName
+            generated = generated.."   [\""..escape(v.Name).."\"] : "..v.ClassName
             if #v:GetChildren() > 1 then
                 generated = generated .. " & {\n"
                 subFunction_dump(v)
@@ -117,7 +120,7 @@ local function generateDtsCode(obj : Instance)
         end
     end
     for _,v in obj:GetChildren() do
-        generated = generated.."   [\""..obj.Name.."\"] : "..obj.ClassName
+        generated = generated.."   [\""..escape(obj.Name).."\"] : "..obj.ClassName
         if #v:GetChildren() < 1 then
             generated ..= ";\n   "
         else
