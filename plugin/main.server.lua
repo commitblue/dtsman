a5accdb0299d0a1d9506a2c70c78255e9ab31185
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-16 20:03:23 +0200

    o

--- plugin/main.server.lua
+++ plugin/main.server.lua
@@ -144,7 +144,7 @@ while true do
             }
             print(state.portText)
             local refToObject = getGlobalFromString(state.object)
-            local success, result = pcall(httpService.GetAsync, string.format("https://localhost:%s/", state.portText))
+            local success, result = pcall(httpService.GetAsync, httpService, string.format("https://localhost:%s/", state.portText))
             print(result)
             if success and result == "dtsman" then
                 success, result = pcall(httpService.RequestAsync, {
