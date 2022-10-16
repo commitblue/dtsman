542c29762113b796fe9fd057de2a569f7fc202fc
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-16 21:34:54 +0200

    ok

--- plugin/main.server.lua
+++ plugin/main.server.lua
@@ -32,7 +32,11 @@ end)
 local started = false
 dtsGui = roactrodux.connect(
     function(state)
-        return state
+        return {
+            portText = state.portText,
+            object = state.object,
+            status = state.status
+        }
     end,
     function(dispatch)
         return {
