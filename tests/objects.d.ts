115bf582af6313fc1963ffa8ef269e480552781d
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-17 22:10:22 +0200

    works

--- tests/objects.d.ts
+++ tests/objects.d.ts
@@ -1,11 +1,10 @@
-type Baseplate = Part & {
-      ["Baseplate"] : Part & {
-      ["ok"] : Part;
-         ["z"] : Part & {
-         ["Part"] : Part & {
-            ["Part"] : Part;
-            
-};      
-};   
+type cubicbeziercurve = Folder & {
+      ["cubicbeziercurve"] : Folder;
+      ["cubicbeziercurve"] : Folder & {
+      ["p1"] : Part;
+         ["p2"] : Part;
+         ["p3"] : Part;
+         ["p4"] : Part;
+      
 };
 };
